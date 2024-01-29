f711b1f5514d9d12e4b39b9c9e954f1dc5f76cf5
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-01-29 01:45:57 +0100

    Add test parametrization using yml file

--- test/webdriver_test_spec.rb
+++ test/webdriver_test_spec.rb
@@ -2,27 +2,32 @@
 
 require 'rspec'
 require 'selenium-webdriver'
+require 'yaml'
 require_relative './page_objects/duckduckgo/ddg_query_page'
 require_relative './page_objects/duckduckgo/ddg_result_page'
 
+config = YAML.load(File.read("test/config/test_params.yml"), symbolize_names: true)
+
 RSpec.describe 'WebdriverTest' do
   before(:each) do
-    @driver = Selenium::WebDriver.for(:firefox)
+    browser = config[:browser].to_sym || :chrome
+    @driver = Selenium::WebDriver.for(browser)
     @driver.manage.delete_all_cookies
   end
 
   it 'should open browser and navigate to google.com and search for something' do
+    query = config[:query] || "something"
 
     DdgQueryPage.new(@driver)
       .open_page
-      .search("ruby rspec selenium")
+      .search(query)
 
     results = DdgResultPage.new(@driver)
       .get_first_ten_results
 
 
     # Temporary code
-    sleep 5
+    sleep 2
 
     puts "Browser window name is ""#{@driver.title}"""
   end
